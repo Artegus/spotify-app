4314af9572a804a5cb1bd3e1729757b8f3ecf6ad
Gabriel Agustín Uriarte <gabri02.uri@gmail.com>
2020-11-09 00:11:42 +0100

    added comments

--- JS/web-api-auth/spotify_main.js
+++ JS/web-api-auth/spotify_main.js
@@ -10,7 +10,7 @@ const APPController = (function(APICtrl, UICtrl, AUDIOCtrl){
         const token = await APICtrl.getToken()
         // Store token
         UICtrl.storeToken(token);
-        // Get playlists 'toplist'
+        // Get playlists 'toplists'
         const toplistPlaylists = await APICtrl.getCategoryPlaylists(token, 'toplists')
         // Add the elements to thhe interface 
         toplistPlaylists.forEach((playlist) => {
@@ -61,7 +61,6 @@ const APPController = (function(APICtrl, UICtrl, AUDIOCtrl){
         // Posición checkbox
         var posicionFila = 0; // Apañado para ir poniendo el chekcbox en cada una de las filas
         // Add track to tbody
-        console.log(tracks)
         tracks.forEach((track) => {
             
             // Get info for each track
@@ -79,13 +78,14 @@ const APPController = (function(APICtrl, UICtrl, AUDIOCtrl){
                     artists : [{ name : artist, external_urls : { spotify: urlSpotifyArtist} }]
                 }
             } = track
-            // Itero la posición.
+            // Iterate position of checkbox.
             posicionFila += 1;
             UICtrl.createTrack(nameTrack, artist, Math.floor(duration * 0.001), nameAlbum, date, url_song, trackEndPoint, urlSpotifySong, urlSpotifyArtist, posicionFila)
         })
         // Add button to add tracks to playlist
         UICtrl.addButtonToAddTracksToPlaylist();
     })
+
     // Play song and display img of song
     DOMcontainers.tracksPlaylist.addEventListener('click', async (e) => {
         // Get stored token
@@ -99,7 +99,6 @@ const APPController = (function(APICtrl, UICtrl, AUDIOCtrl){
             // Get trackEndPoint
             const trackEndPoint = getSongUrl(trackSelected)
             const track = await APICtrl.getTrack(token, trackEndPoint)
-            console.log(track)
             // Get information of track
             const {
                 album : {
@@ -109,16 +108,9 @@ const APPController = (function(APICtrl, UICtrl, AUDIOCtrl){
                 artists : [{name : nameArtist, id : artistId}],
                 name : nameTrack
             } = track
-
+            // Create track preview
             UICtrl.createTrackPreview(nameTrack, urlSpotifyAlbum, nameArtist, imgUrl, artistId)
         }
-        // Get song preview and reproduce
-        //playAudio(trackSelected);
-        // Get trackEndPoint 
-        //const trackEndPoint = getSongUrl(trackSelected);
-        //console.log(trackEndPoint)
-        // Reproduce song
-        //AUDIOCtrl.playAudioHTML(track_preview_url)
     })
 
     
@@ -129,7 +121,7 @@ const APPController = (function(APICtrl, UICtrl, AUDIOCtrl){
         AUDIOCtrl.playAudioHTML(track_preview_url)
     }
 
-    const getSongUrl = (trackSelected) => { 
+    const getSongUrl = (trackSelected) => {
         return trackSelected.dataset.song;
     }
 
