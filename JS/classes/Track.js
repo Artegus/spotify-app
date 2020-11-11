5bc5269abcebb7d3b639097b45ed2ae5537a96a8
Gabriel Agustín Uriarte <gabri02.uri@gmail.com>
2020-11-11 22:41:03 +0100

    added new properties

--- JS/classes/Track.js
+++ JS/classes/Track.js
@@ -1,10 +1,13 @@
 class Track {
     
-    constructor (name, artist, album, duration) {
+    constructor (name, artist, album, duration, dateAdded, urlPreview, trackEndPoint) {
         this._name = name;
         this._artist = artist;
         this._album = album;
         this._duration = duration;
+        this._dateAdded = dateAdded;
+        this._urlPreview = urlPreview;
+        this._trackEndPoint = trackEndPoint;
     }
 
     // Getters
