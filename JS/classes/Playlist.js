ee8abf24fd709d2fbf4b4a6404d88aa2a92fb819
Gabriel Agustín Uriarte <gabri02.uri@gmail.com>
2020-11-12 01:48:34 +0100

    added functions to order

--- JS/classes/Playlist.js
+++ JS/classes/Playlist.js
@@ -38,15 +38,47 @@ class Playlist {
         console.log(this._listOfTracks);
     }
 
-    orderByDuration () {
+    orderByDurationASC () {
         this._listOfTracks.sort((track1, track2) => track1.duration - track2.duration);
     }
+    orderByDurationDES() {
+        this._listOfTracks.sort((track1, track2) => track2.duration - track1.duration);
+    }
 
-    orderByName () {
+    orderByNameASC () {
         this._listOfTracks.sort((track1, track2) => track1.name.localeCompare(track2.name))
     }
-
-    orderByAlbum () {
+    orderByNameDES() {
+        this._listOfTracks.sort((track1, track2) => track2.name.localeCompare(track1.name))
+    }
+    orderByAlbumASC () {
         this._listOfTracks.sort((track1, track2) => track1.album.localeCompare(track2.album))
     }
+    orderByAlbumDES () {
+        this._listOfTracks.sort((track1, track2) => track2.album.localeCompare(track1.album))
+    }
+    orderByAlbumASC () {
+
+    }
+    /**
+     * 
+     * @param {string} property 
+     * @param {number} typeOfOrder 0 for ascending order and 1 for descending order 
+     */
+    orderBy (property, typeOfOrder) {
+        property == 'name' ? typeOfOrder === 0 ? 
+            this._listOfTracks.sort((track1, track2) => track1.name.localeCompare(track2.name)) 
+            : this._listOfTracks.sort((track1, track2) => track2.name.localeCompare(track1.name)) 
+        : property == 'duration' ? typeOfOrder === 0 ?
+            this._listOfTracks.sort((track1, track2) => track1.duration - track2.duration)
+            : this._listOfTracks.sort((track1, track2) =>track2.duration - track1.duration)
+        : property == 'album' ? typeOfOrder === 0 ? 
+            this._listOfTracks.sort((track1, track2) => track1.album - track2.album)
+            : this._listOfTracks.sort((track1, track2) => track2.album - track1.album)
+        : typeOfOrder === 0 ? // Last is for artist
+            this._listOfTracks.sort((track1, track2) => track1.artist - track2.artist)
+            : this._listOfTracks.sort((track1, track2) => track2.artist - track1.artist)
+    }
+
+
 }
