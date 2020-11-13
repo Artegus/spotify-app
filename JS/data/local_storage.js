f3a6c43fce7c8221dab6daf5d2f3db547834c30a
Gabriel Agustín Uriarte <gabri02.uri@gmail.com>
2020-11-13 18:51:23 +0100

    little modifie in the names of the functions

--- JS/data/local_storage.js
+++ JS/data/local_storage.js
@@ -1,24 +1,24 @@
 
 const LSController = (function () {
 
-    const _saveUserPlaylist = (user) => {
-        const playlist = JSON.stringify(user.playlist) // convert object to JSON string
-        localStorage.setItem('playlists', playlist)
+    const _saveUserPlaylists = (user) => {
+        const playlists = JSON.stringify(user.playlists) // convert object to JSON string
+        localStorage.setItem('playlists', playlists)
     }
 
-    const _getUserPlaylist = () => {
+    const _getUserPlaylists = () => {
         const data = localStorage.getItem('playlists')
-        const playlist = JSON.parse(data) // convert JSON string to JavaScript object o value
+        const playlists = JSON.parse(data) // convert JSON string to JavaScript object o value
 
-        return playlist;
+        return playlists;
     }
 
     return {
-        saverUserPlaylist (user) {
-            return _saveUserPlaylist(user);
+        saverUserPlaylists (user) {
+            return _saveUserPlaylists(user);
         },
-        getUserPlaylist() {
-            return _getUserPlaylist();
+        getUserPlaylists() {
+            return _getUserPlaylists();
         }
     }
 
